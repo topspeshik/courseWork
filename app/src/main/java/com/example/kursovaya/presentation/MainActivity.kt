ef774560e2c8868f0a24b7a31cdaa43e17eabe43
Daniel <speshik222@gmail.com>
2023-04-10 18:58:55 +0800

    doesnt work

--- app/src/main/java/com/example/kursovaya/presentation/MainActivity.kt
+++ app/src/main/java/com/example/kursovaya/presentation/MainActivity.kt
@@ -2,13 +2,53 @@ package com.example.kursovaya.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment.Companion.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.example.kursovaya.R
+import com.example.kursovaya.presentation.Training.StartTrainingAdapter.TrainingFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
 
+
+    lateinit var bottomNav : BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        loadFragment(TrainingFragment())
+        bottomNav = findViewById(R.id.bottomNavigationView)
+        bottomNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> {
+                    loadFragment(MainFragment())
+                    true
+                }
+                R.id.food -> {
+                    loadFragment(FoodFragment())
+                    true
+                }
+                R.id.training -> {
+                    loadFragment(TrainingFragment())
+                    true
+                }
+                else -> {
+                    loadFragment(MainFragment())
+                    true
+                }
+            }
+        }
+
+
+    }
+
+    private  fun loadFragment(fragment: Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container,fragment)
+        transaction.commit()
     }
 }
